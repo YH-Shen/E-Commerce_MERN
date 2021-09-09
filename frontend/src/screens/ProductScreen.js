3084439734fd57c674684e8976e4270871f9a95a
Yuhui Shen <yuhui.shen@hotmail.com>
2021-09-09 14:00:55 -0400

    create qty select and add to cart btn

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap';
 import Rating from '../components/Rating/Rating';
 import Loader from '../components/Loader/Loader';
 import Message from '../components/Message/Message';
 import { listProductDetails } from '../actions/productActions';
 
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ history, match }) => {
+
+    const [qty, setQty] = useState(1);
 
     const dispatch = useDispatch();
 
@@ -23,7 +25,13 @@ const ProductScreen = ({ match }) => {
 
     }, [dispatch, match])
 
-    
+
+    const addToCartHandler = () => {
+        // use props.history.push to redirect
+        //  also send the qty throgh query string
+        history.push(`/cart/${match.params.id}?qty=${qty}`);
+    }
+
     return (
         <>
             <Link className="btn btn-secondary my-3" to="/"> Go Back </Link>
@@ -68,8 +76,28 @@ const ProductScreen = ({ match }) => {
                                 </Row>
                             </ListGroup.Item>
 
+                            {product.countInStock > 0 && (
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Qty:</Col>
+                                        <Col>
+                                            <Form.Control as="select" className="form-select" value={qty} 
+                                                onChange={(e) => {setQty(e.target.value)}} >
+                                                {[...Array(product.countInStock).keys()].map(x => (
+                                                    <option key={x + 1} value={x + 1}>{x + 1}</option>
+                                                ))}
+                                            </Form.Control>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                            )}
+
                             <ListGroup.Item className="d-grid gap-2">
-                                <Button variant="primary" type="button" disabled={product.countInStock === 0}>
+                                <Button 
+                                    onClick={addToCartHandler}
+                                    variant="primary" 
+                                    type="button" 
+                                    disabled={product.countInStock === 0}>
                                     Add To Cart
                                 </Button>
                             </ListGroup.Item>
